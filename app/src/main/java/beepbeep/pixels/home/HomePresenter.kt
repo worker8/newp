b96ef57f437b506521f1cf8b50dc77d1e45c1a13
Tan Jun Rong <tanjunrong@gmail.com>
2017-09-24 22:43:14 +0900

    refactor

--- app/src/main/java/beepbeep/pixels/home/HomePresenter.kt
+++ app/src/main/java/beepbeep/pixels/home/HomePresenter.kt
@@ -13,6 +13,8 @@ import beepbeep.pixels.shared.extension.upBackgroundThread
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
+import net.dean.jraw.models.Listing
+import net.dean.jraw.models.Submission
 
 
 class HomePresenter(val input: HomeContract.Input, val repo: HomeRepoInterface = HomeRepo()) : LifecycleObserver {
@@ -27,6 +29,14 @@ class HomePresenter(val input: HomeContract.Input, val repo: HomeRepoInterface =
             get() = onDataLoaded.hide()
     }
 
+    private fun onDataLoaded(): (Pair<Boolean, Listing<Submission>>) -> Unit {
+        return { (started, submissionListing) ->
+            submissionListing.forEachIndexed { index, submission ->
+                PixelsApplication.pixelsCache?.submissionDao()?.insert(SubmissionCache(submission))
+            }
+        }
+    }
+
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate() {
         val initialTrigger = PublishSubject.create<Unit>()
@@ -35,28 +45,7 @@ class HomePresenter(val input: HomeContract.Input, val repo: HomeRepoInterface =
                 .merge(initialTrigger, input.retry, input.loadMore)
                 .publish()
                 .apply {
-                    map { input.isConnectedToInternet() }
-                            .filter { it }
-                            .filter { repo.isRedditClientAuthed() }
-                            .map { repo.getPaginator() }
-                            .flatMap { repo.data(it) }
-                            .doOnNext { (started, submissionListing) ->
-                                if (!started) {
-                                    repo.deleteAllFromSub()
-                                }
-                            }
-                            .subscribe(
-                                    // onNext
-                                    { (started, submissionListing) ->
-                                        submissionListing.forEachIndexed { index, submission ->
-                                            PixelsApplication.pixelsCache?.submissionDao()?.insert(SubmissionCache(submission))
-                                        }
-                                    },
-                                    // onError
-                                    {
-                                        it.printStackTrace()
-                                    })
-
+                    // Initial flow
                     map { input.isConnectedToInternet() }
                             .filter { it }
                             .filter { !repo.isRedditClientAuthed() }
@@ -69,18 +58,22 @@ class HomePresenter(val input: HomeContract.Input, val repo: HomeRepoInterface =
                             }
                             .upBackgroundThread()
                             .downBackgroundThread()
-                            .subscribe(
-                                    // onNext
-                                    { (started, submissionListing) ->
-                                        submissionListing.forEachIndexed { index, submission ->
-                                            PixelsApplication.pixelsCache?.submissionDao()?.insert(SubmissionCache(submission))
-                                        }
-                                    },
-                                    // onError
-                                    {
-                                        it.printStackTrace()
-                                    })
+                            .subscribe(onDataLoaded())
 
+                    // load more flow
+                    map { input.isConnectedToInternet() }
+                            .filter { it }
+                            .filter { repo.isRedditClientAuthed() }
+                            .map { repo.getPaginator() }
+                            .flatMap { repo.data(it) }
+                            .doOnNext { (started, submissionListing) ->
+                                if (!started) {
+                                    repo.deleteAllFromSub()
+                                }
+                            }
+                            .subscribe(onDataLoaded())
+
+                    // when offline
                     map { !input.isConnectedToInternet() }
                             .filter { it }
                             .upBackgroundThread()
