3919512eb6f7ea0378c0c6f8fe6c1d46d5fb359e
Tan Jun Rong <tanjunrong@gmail.com>
2017-09-30 01:23:24 +0900

    add mockito lib; upgrade kotlin version

---
+++ app/src/test/java/beepbeep/pixels/home/HomePresenter.kt
@@ -0,0 +1,11 @@
+package beepbeep.pixels.home
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class HomePresenter {
+    @Test
+    fun firstTest() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
