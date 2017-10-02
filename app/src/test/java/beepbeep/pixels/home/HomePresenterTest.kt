44e26344060fa0ae7e959ed9c90f4cdd2b2a9e70
Tan Jun Rong <tanjunrong@gmail.com>
2017-10-02 22:48:21 +0900

    first test working! ^3^

--- app/src/test/java/beepbeep/pixels/home/HomePresenterTest.kt
+++ app/src/test/java/beepbeep/pixels/home/HomePresenterTest.kt
@@ -1,7 +1,14 @@
 package beepbeep.pixels.home
 
+import beepbeep.pixels.cache.submission.SubmissionCache
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
-import org.junit.Assert.assertEquals
+import net.dean.jraw.models.Listing
+import net.dean.jraw.models.Submission
+import net.dean.jraw.paginators.SubredditPaginator
+import org.junit.Before
 import org.junit.Test
 
 class HomePresenterTest {
@@ -10,6 +17,12 @@ class HomePresenterTest {
     var _refresh = PublishSubject.create<Any>()
     var _retry = PublishSubject.create<Unit>()
 
+    var _initGuestRedditClient = PublishSubject.create<SubredditPaginator>()
+    var _getPaginator = PublishSubject.create<SubredditPaginator>()
+    var _isRedditClientAuthed = false
+    var _data = PublishSubject.create<Pair<Boolean, Listing<Submission>>>()
+    var _bindToDb = PublishSubject.create<List<SubmissionCache>>()
+
     val input = object : HomeContract.Input {
         override fun isConnectedToInternet() = _isConnectedToInternet
         override val loadMore = _loadMore.hide()
@@ -17,9 +30,37 @@ class HomePresenterTest {
         override val retry = _retry.hide()
     }
 
-    val presenterUT = HomePresenter(input)
+    val repo = object : HomeRepoInterface {
+        override fun initGuestRedditClient() = _initGuestRedditClient.hide()
+        override fun getPaginator() = _getPaginator.hide()
+        override fun isRedditClientAuthed() = _isRedditClientAuthed
+        override fun reset() {}
+        override fun loadMore() {}
+        override fun data(paginatorObs: Observable<SubredditPaginator>) = _data.hide()
+        override fun destroy() {}
+        override fun bindToDb() = _bindToDb.toFlowable(BackpressureStrategy.BUFFER)
+        override fun deleteAllFromSub() {}
+        override fun getBackgroundThread() = Schedulers.trampoline()
+        override fun getMainUiThread() = Schedulers.trampoline()
+    }
+
+    val presenterUT = HomePresenter(input, repo)
+
+    @Before
+    fun setup() {
+        //presenterUT.onCreate()
+    }
+
     @Test
-    fun firstTest() {
-        assertEquals(4, (2 + 2).toLong())
+    fun noInternetTest() {
+        // arrange
+        _isConnectedToInternet = false
+        val testObs = presenterUT.output.showNoInternetSnackbar.test()
+
+        // action
+        presenterUT.onCreate()
+
+        // assert
+        testObs.assertNoErrors().assertValueCount(1)
     }
 }
